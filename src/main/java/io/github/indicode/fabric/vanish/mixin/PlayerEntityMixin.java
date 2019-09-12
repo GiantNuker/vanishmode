46c279d85926aed57332ee36ddd3143ba45f9d66
Indigo A <indigoa.coding@gmail.com>
2019-09-12 16:36:25 -0700

    remove colission

---
+++ src/main/java/io/github/indicode/fabric/vanish/mixin/PlayerEntityMixin.java
@@ -0,0 +1,43 @@
+package io.github.indicode.fabric.vanish.mixin;
+
+import com.mojang.authlib.GameProfile;
+import io.github.indicode.fabric.vanish.VanishDB;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+
+/**
+ * @author Indigo Amann
+ */
+@Mixin(ServerPlayerEntity.class)
+public abstract class PlayerEntityMixin extends PlayerEntity {
+    public PlayerEntityMixin(World world_1, GameProfile gameProfile_1) {
+        super(world_1, gameProfile_1);
+    }
+
+    @Override
+    public Box getBoundingBox() {
+        if (VanishDB.isVanished(getGameProfile().getId())) {
+            return new Box(0, 0, 0, 0, 0, 0);
+        } else {
+            return super.getBoundingBox();
+        }
+    }
+    @Override
+    public boolean isPushable() {
+        return VanishDB.isVanished(getGameProfile().getId()) ? false : super.isPushable();
+    }
+    @Override
+    public void pushAwayFrom(Entity entity_1) {
+        if (VanishDB.isVanished(getGameProfile().getId())) return;
+        else super.pushAwayFrom(entity_1);
+    }
+}
