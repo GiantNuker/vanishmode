5ac054e46935ab7e632771724f11493aab68d30e
Indigo A <indigoa.coding@gmail.com>
2019-09-15 23:31:42 -0700

    Mobs stop going after player w/o invincibility(only using mobs_ignore)

---
+++ src/main/java/io/github/indicode/fabric/vanish/mixin/MobEntityMixin.java
@@ -0,0 +1,25 @@
+package io.github.indicode.fabric.vanish.mixin;
+
+import io.github.indicode.fabric.vanish.VanishDB;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * @author Indigo Amann
+ */
+@Mixin(MobEntity.class)
+public class MobEntityMixin {
+    @Inject(method = "canTarget(Lnet/minecraft/entity/LivingEntity;)Z", at = @At("HEAD"), cancellable = true)
+    public void cantTarget(LivingEntity entity, CallbackInfoReturnable ci) {
+        if (entity instanceof PlayerEntity && VanishDB.isVanished(((PlayerEntity) entity).getGameProfile().getId()) && VanishDB.getOrCreateSettings(((PlayerEntity) entity).getGameProfile().getId()).mobs_ignore) {
+            ci.setReturnValue(false);
+        }
+    }
+}
---
+++ src/main/java/io/github/indicode/fabric/vanish/mixin/TrackTargetGoalMixin.java
@@ -0,0 +1,23 @@
+package io.github.indicode.fabric.vanish.mixin;
+
+import io.github.indicode.fabric.vanish.VanishDB;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.TrackTargetGoal;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/**
+ * @author Indigo Amann
+ */
+@Mixin(TrackTargetGoal.class)
+public class TrackTargetGoalMixin {
+    @Redirect(method = "shouldContinue", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;isAlive()Z"))
+    public boolean isMobIgnoreVanished(LivingEntity entity) {
+        if (entity instanceof PlayerEntity && VanishDB.isVanished(((PlayerEntity) entity).getGameProfile().getId()) && VanishDB.getOrCreateSettings(((PlayerEntity) entity).getGameProfile().getId()).mobs_ignore) {
+            return false;
+        } else return entity.isAlive();
+    }
+}
