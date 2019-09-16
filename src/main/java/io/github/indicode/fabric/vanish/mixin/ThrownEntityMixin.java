b2d93115a015f036a6582a3589c4dc1269939583
Indigo A <indigoa.coding@gmail.com>
2019-09-16 01:19:03 -0700

    Projectiles(Arrows) & Throwables(Snowball, Egg, Potion) clip through vanished player.

---
+++ src/main/java/io/github/indicode/fabric/vanish/mixin/ThrownEntityMixin.java
@@ -0,0 +1,24 @@
+package io.github.indicode.fabric.vanish.mixin;
+
+import io.github.indicode.fabric.vanish.VanishDB;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.thrown.ThrownEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/**
+ * @author Indigo Amann
+ */
+@Mixin(ThrownEntity.class)
+public class ThrownEntityMixin {
+    @Redirect(method = "method_18081", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;isSpectator()Z"))
+    private static boolean isVanished(Entity playerEntity) {
+        return playerEntity.isSpectator() || (playerEntity instanceof PlayerEntity && VanishDB.isVanished(((PlayerEntity) playerEntity).getGameProfile().getId()));
+    }
+    @Redirect(method = "method_18080", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;isSpectator()Z"))
+    private boolean isVanished2(Entity playerEntity) {
+        return playerEntity.isSpectator() || (playerEntity instanceof PlayerEntity && VanishDB.isVanished(((PlayerEntity) playerEntity).getGameProfile().getId()));
+    }
+}
