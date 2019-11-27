cd8d689ce174871ff6dc1817b40710b8a43c4b14
Indigo Amann <indigoa.coding@gmail.com>
2019-11-27 15:48:38 -0800

    Updates to 1.15 along with dependencies. Fresh intermittent bugs!

--- src/main/java/io/github/indicode/fabric/vanish/Vanish.java
+++ src/main/java/io/github/indicode/fabric/vanish/Vanish.java
@@ -1,8 +1,8 @@
 package io.github.indicode.fabric.vanish;
 
+import io.github.indicode.fabric.permissions.PermChangeBehavior;
 import io.github.indicode.fabric.permissions.Permission;
 import io.github.indicode.fabric.permissions.Thimble;
-import io.github.indicode.fabric.permissions.command.CommandPermission;
 import io.github.indicode.fabric.worlddata.WorldDataLib;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -16,16 +16,10 @@ public class Vanish implements ModInitializer {
     @Override
     public void onInitialize() {
         Thimble.permissionWriters.add((map, server) -> {
-            try {
-                map.getPermission("vanish", CommandPermission.class);
-                map.getPermission("vanish.vanish", CommandPermission.class);
-                map.getPermission("vanish.view", CommandPermission.class);
-                map.getPermission("vanish.setting", CommandPermission.class);
-                for (VanishCommand.Setting setting: VanishCommand.Setting.values()) {
-                    map.getPermission("vanish.setting." + setting.id, CommandPermission.class);
-                }
-            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
-                e.printStackTrace();
+            map.registerPermission("vanish.vanish", PermChangeBehavior.UPDATE_COMMAND_TREE);
+            map.registerPermission("vanish.view", PermChangeBehavior.UPDATE_COMMAND_TREE);
+            for (VanishCommand.Setting setting : VanishCommand.Setting.values()) {
+                map.registerPermission("vanish.setting." + setting.id, PermChangeBehavior.UPDATE_COMMAND_TREE);
             }
         });
         WorldDataLib.addIOCallback(new VanishDB.DataStorageHandler());
