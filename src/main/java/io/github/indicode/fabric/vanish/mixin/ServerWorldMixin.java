b6455ec5a003087b9422e14d32cf88b2033b6286
Indigo Amann <indigoa.coding@gmail.com>
2019-12-14 10:27:44 -0800

    Stop block breaking effect

--- src/main/java/io/github/indicode/fabric/vanish/mixin/ServerWorldMixin.java
+++ src/main/java/io/github/indicode/fabric/vanish/mixin/ServerWorldMixin.java
@@ -1,6 +1,7 @@
 package io.github.indicode.fabric.vanish.mixin;
 
 import io.github.indicode.fabric.vanish.VanishDB;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.Packet;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -9,7 +10,9 @@ import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
@@ -25,5 +28,12 @@ public abstract class ServerWorldMixin {
             }
         }
         networkHandler.sendPacket(packet);
+    }
+
+    @Inject(method = "playLevelEvent", at = @At("HEAD"), cancellable = true)
+    private void dontPlayBreakEvent(PlayerEntity player, int eventId, BlockPos blockPos, int data, CallbackInfo ci) {
+        if (eventId == 2001 && player != null && VanishDB.INSTANCE.isVanished(player.getGameProfile().getId())) {
+            ci.cancel();
+        }
     }
 }
